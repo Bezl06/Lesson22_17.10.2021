b9178350128f896646954437dd1233ed91eff19a
Parviz Karimov <bezlpk@gmail.com>
2021-10-22 19:54:49 +0500

    Second commit

--- Context.cs
+++ Context.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
 
-namespace MyDataBase
+namespace MyApp
 {
     public class DataBaseContext : DbContext
     {
-        public DataBaseContext() => Database.EnsureCreated();
+        public DataBaseContext()
+        {
+            Database.EnsureCreated();
+        }
         public DbSet<Client> Clients { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -14,8 +18,70 @@ namespace MyDataBase
     }
     public class Client
     {
+        public Client(string name, decimal balance)
+        {
+            Name = name;
+            Balance = balance;
+        }
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Balance { get; set; }
+    }
+    class Table
+    {
+        private string[][] table;
+        private int[] maxLRaw;
+        private int raws = 0;
+        private readonly int cols;
+        public Table(params string[] firstRaw)
+        {
+            cols = firstRaw.Length;
+            maxLRaw = new int[cols];
+            for (int i = 0; i < cols; i++)
+                maxLRaw[i] = 0;
+            AddRaw(firstRaw);
+        }
+        public void AddRaw(params string[] raw)
+        {
+            Array.Resize<string[]>(ref table, raws + 1);
+            table[^1] = new string[cols];
+            for (int i = 0; i < cols; i++)
+            {
+                table[raws][i] = raw[i];
+                maxLRaw[i] = raw[i].Length > maxLRaw[i] ? raw[i].Length : maxLRaw[i];
+            }
+            raws++;
+        }
+        public void ShowTable()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            StringBuilder sb = new StringBuilder(Sum() + cols + 1);
+            sb.Append('|').Append(' ', maxLRaw[0] - table[0][0].Length).Append(table[0][0]).Append('|');
+            for (int i = 1; i < cols; i++)
+                sb.Append(' ', maxLRaw[i] - table[0][i].Length).Append(table[0][i]).Append('|');
+            System.Console.WriteLine(sb);
+            sb.Clear();
+            sb.Append('|').Append('-', maxLRaw[0]).Append('|');
+            for (int i = 1; i < cols; i++)
+                sb.Append('-', maxLRaw[i]).Append('|');
+            System.Console.WriteLine(sb);
+            sb.Clear();
+            for (int i = 1; i < raws; i++)
+            {
+                sb.Append('|').Append(' ', maxLRaw[0] - table[i][0].Length).Append(table[i][0]).Append('|');
+                for (int j = 1; j < cols; j++)
+                    sb.Append(' ', maxLRaw[j] - table[i][j].Length).Append(table[i][j]).Append('|');
+                System.Console.WriteLine(sb);
+                sb.Clear();
+            }
+            Console.ResetColor();
+        }
+        private int Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < cols; i++)
+                sum += ++maxLRaw[i];
+            return sum;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,23 +1,54 @@
 ﻿using System;
-using MyDataBase;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace MyApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
+        {
+            await Insert(new Client("Ahmad", 100), new Client("Umar", 200), new Client("Ikrom", 300));
+            await Select();
+            System.Console.WriteLine();
+            await Update(new Client("Jovid", 500), 1);
+            await Select();
+            System.Console.WriteLine();
+            await Delete(1);
+            await Select();
+        }
+        public async static Task Insert(params Client[] clients)
         {
             using var db = new DataBaseContext();
-            db.Clients.AddRange(
-                new Client() { Name = "Hokim", Balance = 100 },
-                new Client() { Name = "Osim", Balance = 200 },
-                new Client() { Name = "Umed", Balance = 300 }
-            );
-            db.SaveChanges();
-            foreach (var item in db.Clients)
-            {
-                System.Console.WriteLine($"Name : {item.Name}, Balance : {item.Balance}");
-            }
+            await db.Clients.AddRangeAsync(clients);
+            await db.SaveChangesAsync();
+        }
+        public async static Task Select()
+        {
+            using var db = new DataBaseContext();
+            Table table1 = new Table("ID", "Name", "Balance");
+            await foreach (var item in db.Clients.AsAsyncEnumerable())
+                table1.AddRaw(item.Id.ToString(), item.Name, item.Balance.ToString());
+            table1.ShowTable();
+        }
+        public async static Task Update(Client updated, int id)
+        {
+            using var db = new DataBaseContext();
+            Client client = await db.Clients.FirstAsync(x => x.Id == id);
+            if (client == null) return;
+            client.Balance = updated.Balance;
+            client.Name = updated.Name;
+            await db.SaveChangesAsync();
+        }
+        public async static Task Delete(int id)
+        {
+            using var db = new DataBaseContext();
+            Client client = await db.Clients.FirstAsync(x => x.Id == id);
+            if (client == null) return;
+            db.Clients.Remove(client);
+            await db.SaveChangesAsync();
         }
     }
 }
